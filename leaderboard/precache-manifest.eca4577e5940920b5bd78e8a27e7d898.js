15110c7c3036af8efb2e34bd68180429167d42eb
david <beetz12@gmail.com>
2019-04-09 02:18:56 -0400

    added leaderboard

---
+++ leaderboard/precache-manifest.eca4577e5940920b5bd78e8a27e7d898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6651ffb14f582352685e",
+    "url": "/static/css/main.e29aaf1b.chunk.css"
+  },
+  {
+    "revision": "6651ffb14f582352685e",
+    "url": "/static/js/main.ed28514c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "979e1bb5e6e004e813fb",
+    "url": "/static/js/2.3ec30da2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "08924c5bc451268a825dfda060f8baae",
+    "url": "/index.html"
+  }
+];
