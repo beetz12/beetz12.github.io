5eb31e7fc6bb2053581dc55649891052747773d1
david <beetz12@gmail.com>
2019-04-09 01:44:11 -0400

    my initial commit

---
+++ precache-manifest.bbf33c782954336cd1e664ff891f657f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc2a12811774508b923b",
+    "url": "/static/js/main.63510a4c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b59eb3a9e6edc88a9ce0",
+    "url": "/static/js/2.d62a5987.chunk.js"
+  },
+  {
+    "revision": "7f2dd7106fbd52a13aea8f81db95674b",
+    "url": "/index.html"
+  }
+];
